1311f9d98a8ccf2c99a49014809eef6f07e8cce4
jorge06br <jorgebaca06@gmail.com>
2022-05-31 04:41:02 +0000

    [Creando el generador de excusas con JS,CSS,HTML]

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.document.querySelector("#excuse").innerHTML = concat();
+  que;
+  function concat() {
+    let who = ["El gato", "Mi sobrino", "El chocoyo", "La vecina"];
+    let what = ["destruyo", "rompio", "perdio", "robo"];
+    let when = [
+      "cuando estaba comiendo",
+      "durante la noche",
+      "cuando dormía",
+      "mientras me estaba bañando",
+      "cuando fui a comprar pan"
+    ];
+    let excuse1 = who[Math.floor(Math.random() * who.length)];
+    let excuse2 = what[Math.floor(Math.random() * what.length)];
+    let excuse3 = when[Math.floor(Math.random() * when.length)];
+    return excuse1 + " " + excuse2 + " mi tarea " + excuse3;
+  }
 };
